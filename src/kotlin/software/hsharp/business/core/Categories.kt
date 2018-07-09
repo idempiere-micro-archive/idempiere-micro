8a1941d408346882453d0ef0ab084a8412852ae9
davidpodhola <david@podhola.net>
2018-07-09 11:11:59 +0200

    Squashed 'subprojects/software.hsharp.business.core/' content from commit 39f97fa
    git-subtree-dir: subprojects/software.hsharp.business.core
    git-subtree-split: 39f97fa86b05a39c9663b57048a7a491f0ab2bc4

---
+++ src/kotlin/software/hsharp/business/core/Categories.kt
@@ -0,0 +1,44 @@
+package software.hsharp.business.core
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import software.hsharp.business.models.ICategory
+
+object crm_category : IntIdTable(columnName = "category_id") {
+    val ad_client_id = integer("ad_client_id")
+    val ad_org_id = integer("ad_org_id")
+    val isactive = varchar("isactive", 1)
+    val created = datetime("created")
+    val createdby = integer("createdby")
+    val updated = datetime("updated")
+    val updatedby = integer("updatedby")
+    val category_uu= varchar("category_uu", 36)
+
+    val name = varchar("name", 60)
+    val searchKey = varchar("value", 60)
+}
+
+class CategoryModel(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<CategoryModel>(crm_category)
+
+    var category_Id by crm_category.id
+    var AD_Client_Id by crm_category.ad_client_id
+    var AD_Org_Id by crm_category.ad_org_id
+    var IsActive by crm_category.isactive
+    var Created by crm_category.created
+    var CreatedBy by crm_category.createdby
+    var Updated by crm_category.updated
+    var UpdatedBy by crm_category.updatedby
+    var category_Uu by crm_category.category_uu
+    var name by crm_category.name
+    var searchKey by crm_category.searchKey
+}
+
+data class Category(override val Key: Int, override val name : String) : ICategory {
+    override val ID: String
+        get() = ""+Key
+}
+
+class Categories
---
+++ src/kotlin/software/hsharp/business/core/iDempiereEntities.kt
@@ -0,0 +1,62 @@
+package software.hsharp.business.core
+
+import org.idempiere.common.util.DB
+import org.idempiere.common.util.Env
+import java.sql.ResultSet
+import java.util.*
+
+abstract class iDempiereEntities<T, I> {
+    protected abstract val tableName : String
+
+    protected val allIdsSelect : String
+        get() = """SELECT ${tableName}_id FROM adempiere.$tableName WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0)"""
+    protected val countSelect : String
+        get() = "SELECT COUNT(*) FROM adempiere.$tableName WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0)"
+
+    protected abstract fun getEntityById(ctx : Properties, id : Int ) : T?
+    protected abstract fun convertToDTO(t: T) : I
+
+    protected fun getAllData() : List<I> {
+        var dataFromDB : ArrayList<T> = arrayListOf()
+        val connection = DB.getConnectionRO()
+        val ctx = Env.getCtx()
+        val ad_Client_ID = Env.getAD_Client_ID(ctx)
+        val ad_Org_ID = Env.getAD_Org_ID(ctx)
+        val queryIdCommand = allIdsSelect
+        val stmt = connection.prepareStatement(queryIdCommand)
+        stmt.setInt(1, ad_Client_ID)
+        stmt.setInt(2, ad_Org_ID)
+        val rs = stmt.executeQuery()
+        while (rs.next()) {
+            val id = rs.getInt(1)
+            val data : T? = getEntityById( ctx, id )
+            if (data!=null) dataFromDB.add(data)
+        }
+        rs.close()
+        return dataFromDB.map { convertToDTO(it) }
+    }
+
+    protected fun getById( id : Int ) : I? {
+        val ctx = Env.getCtx()
+        val data = getEntityById( ctx, id ) ?: return null
+        return convertToDTO( data )
+    }
+
+    protected fun getCount() : Int {
+        var result = 0
+        val connection = DB.getConnectionRO()
+        val ctx = Env.getCtx()
+        val ad_Client_ID = Env.getAD_Client_ID(ctx)
+        val ad_Org_ID = Env.getAD_Org_ID(ctx)
+        val queryIdCommand = allIdsSelect
+        val stmt = connection.prepareStatement(queryIdCommand)
+        stmt.setInt(1, ad_Client_ID)
+        stmt.setInt(2, ad_Org_ID)
+        val rs = stmt.executeQuery()
+        while (rs.next()) {
+            result = rs.getInt(1)
+        }
+        rs.close()
+        return result
+    }
+}
