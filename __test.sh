b28e737ab32cbe241aad5be4ef1e0c8e60242360
davidpodhola <david@podhola.net>
2018-07-09 14:55:57 +0200

    we cannot have two software.hsharp.db.*.provider.jar in the hot-deploy/subprojects

--- __test.sh
+++ __test.sh
@@ -2,6 +2,7 @@ set -e
 set -o pipefail
 (cd ./runtime;java -Dgosh.args=--nointeractive -jar ./bin/felix.jar &)
 gradle build
+rm -rf ./runtime/bundle/hot-deploy/subprojects/software.hsharp.db.h2.provider.jar
 cd integration_tests
 npm i
 ./run.sh
