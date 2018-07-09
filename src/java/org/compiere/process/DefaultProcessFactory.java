e27e475de53d3ac76c5d2f479328b26ea004bd15
davidpodhola <david@podhola.net>
2018-07-09 11:11:32 +0200

    Squashed 'subprojects/org.compiere.process/' content from commit 4455d12
    git-subtree-dir: subprojects/org.compiere.process
    git-subtree-split: 4455d128f3cc9c501b01bf9bd5609a5d3ef86148

---
+++ src/java/org/compiere/process/DefaultProcessFactory.java
@@ -0,0 +1,76 @@
+package org.compiere.process;
+
+import java.util.logging.Level;
+
+import org.compiere.process.ProcessCall;
+import org.idempiere.common.util.CLogger;
+
+/**
+ * @author hengsin
+ *
+ */
+public class DefaultProcessFactory implements IProcessFactory {
+
+    private final static CLogger log = CLogger.getCLogger(DefaultProcessFactory.class);
+
+    /**
+     * default constructor
+     */
+    public DefaultProcessFactory() {
+    }
+
+    /* (non-Javadoc)
+     * @see org.adempiere.base.IProcessFactory#newProcessInstance(java.lang.String)
+     */
+    @Override
+    public ProcessCall newProcessInstance(String className) {
+        ProcessCall process = null;
+        if (process == null) {
+            //Get Class
+            Class<?> processClass = null;
+            //use context classloader if available
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            if (classLoader != null)
+            {
+                try
+                {
+                    processClass = classLoader.loadClass(className);
+                }
+                catch (ClassNotFoundException ex)
+                {
+                    if (log.isLoggable(Level.FINE))log.log(Level.FINE, className, ex);
+                }
+            }
+            if (processClass == null)
+            {
+                classLoader = this.getClass().getClassLoader();
+                try
+                {
+                    processClass = classLoader.loadClass(className);
+                }
+                catch (ClassNotFoundException ex)
+                {
+                    log.log(Level.WARNING, className, ex);
+                    return null;
+                }
+            }
+
+            if (processClass == null) {
+                return null;
+            }
+
+            //Get Process
+            try
+            {
+                process = (ProcessCall)processClass.newInstance();
+            }
+            catch (Exception ex)
+            {
+                log.log(Level.WARNING, "Instance for " + className, ex);
+                return null;
+            }
+        }
+        return process;
+    }
+
+}
