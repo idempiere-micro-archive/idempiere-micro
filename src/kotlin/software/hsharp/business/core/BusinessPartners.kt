de8d323726f51b731139313efc01e07192e7cf15
davidpodhola <david@podhola.net>
2018-08-18 21:40:21 +0200

    Squashed 'subprojects/software.hsharp.business.core/' changes from 4013f9d..b9adf84
    b9adf84 add flatDiscount code cleanup
    5768635 DTOs implemented
    5998670 cleanup
    0484d12 cleanup
    0e98c83 UPDATE can CAST too
    58589b1 getTypeCast to support INSERT when no column data information available
    
    git-subtree-dir: subprojects/software.hsharp.business.core
    git-subtree-split: b9adf843dddd628b1b47070d38cb404dcf689d1c

--- src/kotlin/software/hsharp/business/core/BusinessPartners.kt
+++ src/kotlin/software/hsharp/business/core/BusinessPartners.kt
@@ -1,84 +1,51 @@
 package software.hsharp.business.core
 
-import org.compiere.crm.MBPartner
-import org.compiere.model.I_C_BPartner
-import org.jetbrains.exposed.dao.EntityID
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.IntIdTable
-import org.osgi.service.component.annotations.Component
-import org.osgi.service.component.annotations.Reference
-import software.hsharp.core.util.Paging
+import org.compiere.model.I_C_BPartner_Location
+import org.compiere.model.I_C_Location
 import software.hsharp.business.models.IBusinessPartner
 import software.hsharp.core.models.IDataSource
 import software.hsharp.core.models.IPaging
-import java.util.*
 import software.hsharp.business.models.IBusinessPartnerLocation
 import software.hsharp.business.models.ILocation
 import software.hsharp.business.services.*
-import software.hsharp.core.services.IServiceRegisterRegister
-
-object c_bpartner : IntIdTable(columnName = "c_bpartner_id") {
-    val ad_client_id = integer("ad_client_id")
-    val ad_org_id = integer("ad_org_id")
-    val isactive = varchar("isactive", 1)
-    val created = datetime("created")
-    val createdby = integer("createdby")
-    val updated = datetime("updated")
-    val updatedby = integer("updatedby")
-    val c_bpartner_uu= varchar("c_bpartner_uu", 36)
-
-    val name = varchar("name", 60)
-    val searchKey = varchar("value", 60)
-
-    val iscustomer = varchar("iscustomer", 1)
-}
-
-open class BusinessPartnerModel(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<BusinessPartnerModel>(c_bpartner)
-
-    var c_bpartner_id by c_bpartner.id
-    var AD_Client_Id by c_bpartner.ad_client_id
-    var AD_Org_Id by c_bpartner.ad_org_id
-    var IsActive by c_bpartner.isactive
-    var Created by c_bpartner.created
-    var CreatedBy by c_bpartner.createdby
-    var Updated by c_bpartner.updated
-    var UpdatedBy by c_bpartner.updatedby
-    var category_Uu by c_bpartner.c_bpartner_uu
-    var name by c_bpartner.name
-    var searchKey by c_bpartner.searchKey
-}
+import java.math.BigDecimal
 
 data class Location(
-        override val CountryName: String?,
-        override val City: String?,
-        override val Postal: String?,
-        override val Address1: String?,
-        override val Address2: String?,
-        override val Address3: String?,
-        override val Address4: String?,
-        override val Address5: String?) : ILocation
+    override val CountryName: String?,
+    override val City: String?,
+    override val Postal: String?,
+    override val Address1: String?,
+    override val Address2: String?,
+    override val Address3: String?,
+    override val Address4: String?,
+    override val Address5: String?
+) : ILocation {
+    constructor(a: I_C_Location): this(a.countryName, a.city, a.postal, a.address1, a.address2, a.address3, a.address4, a.address5)
+}
 
 data class BusinessPartnerLocation(
-        override val Location: ILocation
-) : IBusinessPartnerLocation
+    override val Location: ILocation
+) : IBusinessPartnerLocation {
+    constructor (a: I_C_BPartner_Location): this(Location(a.location))
+}
 
-data class BusinessPartner( 
-    override val Key : Int, 
-    override val name : String, 
-    override val value : String,
-    override val Locations: Array<IBusinessPartnerLocation>    
-     ) : IBusinessPartner {
+data class BusinessPartner(
+    override val Key: Int,
+    override val name: String,
+    override val value: String,
+    override val Locations: Array<IBusinessPartnerLocation>,
+    override val flatDiscount: BigDecimal
+) : IBusinessPartner {
     override val ID: String
-        get() = ""+Key
+        get() = "" + Key
 }
 
 data class BusinessPartnersResult(
-        override val businessPartners : Array<IBusinessPartner>,
-        override val __paging: IPaging?) : IBusinessPartnersResult {
+    override val businessPartners: Array<IBusinessPartner>,
+    override val __paging: IPaging?
+) : IBusinessPartnersResult {
     companion object {
-        val metadata: IDataSource? get () {
+        val metadata: IDataSource? get() {
             /*return DataTable(
                     tableName = "c_bpartner",
                     columns = arrayOf(),
@@ -96,80 +63,9 @@ data class BusinessPartnersResult(
 }
 
 data class BusinessPartnerResult(
-        override val businessPartner : IBusinessPartner?,
-        override val __paging: IPaging?) : IBusinessPartnerResult {
+    override val businessPartner: IBusinessPartner?,
+    override val __paging: IPaging?
+) : IBusinessPartnerResult {
     override val __metadata: IDataSource?
         get() = BusinessPartnersResult.metadata
-}
-
-@Component
-class BusinessPartners : iDempiereEntities<I_C_BPartner, IBusinessPartner>(), IBusinessPartnersImpl {
-    override val name: String
-        get() = "Business Partners Service"
-
-    companion object {
-        public fun convertLocations( t: I_C_BPartner ) : Array<IBusinessPartnerLocation> {
-            return t.locations.map {
-                BusinessPartnerLocation(
-                        Location(
-                                CountryName = it.getLocation().CountryName,
-                                City = it.getLocation().City,
-                                Postal = it.getLocation().Postal,
-                                Address1 = it.getLocation().Address1,
-                                Address2 = it.getLocation().Address2,
-                                Address3 = it.getLocation().Address3,
-                                Address4 = it.getLocation().Address4,
-                                Address5 = it.getLocation().Address5
-                        )
-                )
-            }.toTypedArray()
-        }
-    }
-
-
-    override val tableName: String
-        get() = "c_bpartner"
-
-    override fun getEntityById(ctx: Properties, id: Int): I_C_BPartner? {
-        return MBPartner.get(ctx, id)
-    }
-
-    override fun convertToDTO(t: I_C_BPartner): IBusinessPartner {
-        val result : BusinessPartner =
-                BusinessPartner(
-                        t.c_BPartner_ID,
-                        t.name,
-                        t.value,
-                        convertLocations(t)
-                )
-        return result
-    }
-
-    override fun getAllBusinessPartners(): IBusinessPartnersResult {
-        return BusinessPartnersResult(getAllData().toTypedArray(), Paging(getCount()))
-    }
-
-    override fun getBusinessPartnerById(id: Int): IBusinessPartnerResult {
-        val result = getById(id)
-        return BusinessPartnerResult(result, if(result==null) {
-            Paging(0)
-        } else {
-            Paging(1)
-        } )
-    }
-}
-
-@Component
-class BusinessPartnersRegisterHolder {
-    companion object {
-        var LoginServiceRegister: IBusinessPartnersServiceRegister? = null
-        var loginManager : BusinessPartners = BusinessPartners()
-    }
-
-    @Reference
-    fun setLoginServiceRegister(loginServiceRegister: IBusinessPartnersServiceRegister) {
-        LoginServiceRegister = loginServiceRegister
-        loginServiceRegister.registerService( loginManager )
-    }
-
 }
--- src/kotlin/software/hsharp/business/core/DataService.kt
+++ src/kotlin/software/hsharp/business/core/DataService.kt
@@ -10,8 +10,6 @@ import org.compiere.process.ProcessInfoParameter
 import org.compiere.process.ProcessUtil
 import org.idempiere.common.util.DB
 import org.idempiere.common.util.Env
-import org.idempiere.common.util.Env.getAD_Client_ID
-import org.idempiere.common.util.Trx
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import software.hsharp.core.models.*
@@ -22,55 +20,66 @@ import java.sql.Connection
 import java.sql.ResultSet
 
 data class GetDataResult(
-        override val rs: ResultSet?,
-        override val __metadata: IDataSource?,
-        override val __paging: IPaging?) : IGetDataResult {
+    override val rs: ResultSet?,
+    override val __metadata: IDataSource?,
+    override val __paging: IPaging?,
+    override val rsmdColumnCount: Int,
+    override val cnn: Connection?
+) : IGetDataResult {
     companion object {
-        val empty : IGetDataResult
-            get() = GetDataResult(null, null, null)
+        val empty: IGetDataResult
+            get() = GetDataResult(null, null, null, rsmdColumnCount=0, cnn = null)
     }
 }
 
 data class GetRowResult(
-        override val rs: ResultSet?,
-        override val __metadata: IDataSource?,
-        override val __paging: IPaging?) : IGetRowResultData
+    override val rs: ResultSet?,
+    override val __metadata: IDataSource?,
+    override val __paging: IPaging?,
+    override val rsmdColumnCount: Int,
+    override val cnn: Connection
+) : IGetRowResultData
 
 data class GetRowResultObj(
-        override val o: Any?,
-        override val __metadata: IDataSource?,
-        override val __paging: IPaging?) : IGetRowResultObject
+    override val o: Any?,
+    override val __metadata: IDataSource?,
+    override val __paging: IPaging?,
+    override val rsmdColumnCount: Int,
+    override val cnn: Connection
+) : IGetRowResultObject
 
 data class UpdateDataResult(
-        override val id: Int?,
-        override val __metadata: IDataSource?,
-        override val __paging: IPaging?) : IUpdateDataResult {
+    override val id: Int?,
+    override val __metadata: IDataSource?,
+    override val __paging: IPaging?
+) : IUpdateDataResult {
     companion object {
-        val empty : IUpdateDataResult
+        val empty: IUpdateDataResult
             get() = UpdateDataResult(null, null, null)
     }
 }
 
 data class CreateDataResult(
-        override val id: Int?,
-        override val __metadata: IDataSource?,
-        override val __paging: IPaging?) : ICreateDataResult {
+    override val id: Int?,
+    override val __metadata: IDataSource?,
+    override val __paging: IPaging?
+) : ICreateDataResult {
     companion object {
-        val empty : ICreateDataResult
+        val empty: ICreateDataResult
             get() = CreateDataResult(null, null, null)
     }
 }
 
 data class ExecuteJavaProcessResult(
-        val message : String?,
-        val success : Boolean,
-        val result : Serializable?
+    val message: String?,
+    val success: Boolean,
+    val result: Serializable?
 )
 
 @Component
 class DataService : IDataService {
     override fun getTreeData(connection: Connection, root: ITreeDataDescriptor, orderBy: String, orderByOrder: String, offset: Int, limit: Int, filterName1: String, filterValue1: String, filterName2: String, filterValue2: String): IGetTreeDataResult {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun execute(connection: Connection, procName: String, jsonBody: String): String? {
@@ -78,68 +87,67 @@ class DataService : IDataService {
         val ad_Client_ID = Env.getAD_Client_ID(ctx)
         val ad_Org_ID = Env.getAD_Org_ID(ctx)
         val ad_User_ID = Env.getAD_User_ID(ctx)
-        val cnn = DB.getConnectionRW()
-
-        set_user(cnn, ad_User_ID)
-
-        var parameters : MutableList<ProcessInfoParameter> = mutableListOf(
-                ProcessInfoParameter( "AD_Client_ID", ad_Client_ID.toBigDecimal(), null, null, null ),
-                ProcessInfoParameter( "AD_Org_ID", ad_Org_ID.toBigDecimal(), null, null, null )
+        var parameters: MutableList<ProcessInfoParameter> = mutableListOf(
+                ProcessInfoParameter("AD_Client_ID", ad_Client_ID.toBigDecimal(), null, null, null),
+                ProcessInfoParameter("AD_Org_ID", ad_Org_ID.toBigDecimal(), null, null, null),
+                ProcessInfoParameter("AD_User_ID", ad_User_ID.toBigDecimal(), null, null, null)
         )
         val bodyParams = parse(jsonBody)
         parameters
-                .addAll( 2, bodyParams.map { ProcessInfoParameter(  it.first, it.second, null, null, null ) } )
+                .addAll(3, bodyParams.map { ProcessInfoParameter(it.first, it.second, null, null, null) })
 
         val processInfo = ProcessInfo("Execute Java Process", 0)
         processInfo.aD_Client_ID = ad_Client_ID
         processInfo.aD_User_ID = ad_User_ID
         processInfo.parameter = parameters.toTypedArray()
         processInfo.className = procName
-        val m_trx = Trx.get(Trx.createTrxName("ExecuteJavaProcess"), true)
-        val success = ProcessUtil.startJavaProcess(ctx, processInfo, m_trx, false)
-        val result = ExecuteJavaProcessResult( processInfo.summary, success, processInfo.serializableObject )
+        val success = ProcessUtil.startJavaProcess(ctx, processInfo, null, true)
+        ExecuteJavaProcessResult(processInfo.summary, success, processInfo.serializableObject)
         val mapper = ObjectMapper().registerModule(KotlinModule())
         return mapper.writeValueAsString(processInfo.serializableObject)
     }
 
-    override fun getSchemasSupported(connection:Connection) : Array<String> {
+    override fun getSchemasSupported(connection: Connection): Array<String> {
         return arrayOf("adempiere", "idempiere")
     }
     override val name: String
         get() = "iDempiere Data Service"
 
-    private fun getType(next: Pair<String, Any>, table: IDataTable): String {
-        return table.columns.find({ it.columnName.toLowerCase() == next.first.toLowerCase() })!!.columnType
+    private fun getColumn(next: Pair<String, Any>, table: IDataTable): IDataColumn? {
+        return table.columns.find { it.columnName.toLowerCase() == next.first.toLowerCase() }
+    }
+
+    private fun getTypeCast(next: Pair<String, Any>, table: IDataTable): String {
+        val column = getColumn(next, table)
+        return if (column == null) { "?" } else "CAST(? AS ${column.columnType})"
     }
 
     override fun createData(
-            connection: Connection,
-            tableName: String,
-            table: IDataTable?,
-            fields: MutableList<Pair<String, Any>>,
-            anonymous_call : Boolean
-    ) : ICreateDataResult {
+        connection: Connection,
+        tableName: String,
+        table: IDataTable?,
+        fields: MutableList<Pair<String, Any>>,
+        anonymous_call: Boolean
+    ): ICreateDataResult {
         val ctx = Env.getCtx()
         val ad_Client_ID = Env.getAD_Client_ID(ctx)
         val ad_Org_ID = Env.getAD_Org_ID(ctx)
         val ad_User_ID = Env.getAD_User_ID(ctx)
         val cnn = DB.getConnectionRW()
 
-        set_user(cnn, ad_User_ID)
-
         val tableName_lowerCase = tableName.toLowerCase()
 
         val sql =
-                ( fields.fold( "INSERT INTO \"${tableName_lowerCase}\" ( \"${tableName_lowerCase}_id\",", { total, next -> total + next.first + "," } ) ) +
-                        ( fields.fold(
-                                "ad_client_id, ad_org_id, updatedby, updated, createdby, created ) VALUES ( (SELECT COALESCE(MAX(\"${tableName_lowerCase}_id\"),0) + 1 FROM \"${tableName_lowerCase}\" ),",
-                                { total, next -> "${total}CAST(? AS ${getType(next, table!!)})," } ) ) + " ?, ?, ?, statement_timestamp(), ?, statement_timestamp()) RETURNING ${tableName_lowerCase}_id;";
+                (fields.fold("INSERT INTO \"${tableName_lowerCase}\" ( \"${tableName_lowerCase}_id\",") { total, next -> total + next.first + "," }) +
+                        (fields.fold(
+                                "ad_client_id, ad_org_id, updatedby, updated, createdby, created ) VALUES ( (SELECT COALESCE(MAX(\"${tableName_lowerCase}_id\"),0) + 1 FROM \"${tableName_lowerCase}\" ),"
+                        ) { total, next -> "${total}${getTypeCast(next, table!!)}," }) + " ?, ?, ?, statement_timestamp(), ?, statement_timestamp()) RETURNING ${tableName_lowerCase}_id;"
 
         val statement = cnn.prepareStatement(sql)
         fields.forEachIndexed { index, value ->
             try {
                 statement.setObject(index + 1, value.second)
-            } catch ( ex : Exception ) {
+            } catch (ex: Exception) {
                 throw ex
             }
         }
@@ -152,37 +160,36 @@ class DataService : IDataService {
         val rs = statement.executeQuery()
         connection.commit()
 
-        var result : Int? = null
+        var result: Int? = null
         while (rs.next()) {
             result = rs.getInt(1)
         }
+        cnn.close()
 
         return CreateDataResult(result, null, null)
     }
 
     override fun updateData(
-            connection: Connection,
-            tableName: String,
-            id: Int,
-            fields: MutableList<Pair<String, Any>>,
-            anonymous_call : Boolean
-    ) : IUpdateDataResult {
+        connection: Connection,
+        tableName: String,
+        table: IDataTable?,
+        id: Int,
+        fields: MutableList<Pair<String, Any>>,
+        anonymous_call: Boolean
+    ): IUpdateDataResult {
         val ctx = Env.getCtx()
         val ad_Client_ID = Env.getAD_Client_ID(ctx)
         val ad_Org_ID = Env.getAD_Org_ID(ctx)
-        val ad_User_ID = Env.getAD_User_ID(ctx)
         val cnn = DB.getConnectionRW()
-
-        set_user(cnn, ad_User_ID)
 
         val tableName_lowerCase = tableName.toLowerCase()
 
         val sql =
-                ( fields.fold( "UPDATE \"${tableName_lowerCase}\" SET ", { total, next -> total + "${next.first}=?," } ) ) +
-                        "ad_client_id = ?, ad_org_id = ? WHERE ${tableName_lowerCase}_id = ? RETURNING ${tableName}_id;";
+                (fields.fold("UPDATE \"${tableName_lowerCase}\" SET ", { total, next -> total + "${next.first}=${getTypeCast(next, table!!)}," })) +
+                        "ad_client_id = ?, ad_org_id = ? WHERE ${tableName_lowerCase}_id = ? RETURNING ${tableName}_id;"
 
         val statement = cnn.prepareStatement(sql)
-        fields.forEachIndexed { index, value -> statement.setObject( index + 1, value.second ) }
+        fields.forEachIndexed { index, value -> statement.setObject(index + 1, value.second) }
 
         val fieldsCount = fields.count()
         statement.setInt(fieldsCount + 1, ad_Client_ID)
@@ -192,28 +199,20 @@ class DataService : IDataService {
         val rs = statement.executeQuery()
         // cnn.commit() <- auto commit
 
-        var result : Int? = null
+        var result: Int? = null
         while (rs.next()) {
             result = rs.getInt(1)
         }
+        cnn.close()
 
         return UpdateDataResult(result, null, null)
-
-    }
-
-    private fun set_user(cnn:Connection, ad_User_ID: Int) {
-        val identitySelect = "SELECT set_user(?)"
-        val identityStatement = cnn.prepareStatement(identitySelect)
-        identityStatement.setInt(1, ad_User_ID)
-        val identityRs = identityStatement.executeQuery()
-        while(identityRs.next()) {}
     }
 
     override fun getRow(
-            connection: Connection,
-            tableName: String,
-            id: Int,
-            anonymous_call : Boolean
+        connection: Connection,
+        tableName: String,
+        id: Int,
+        anonymous_call: Boolean
     ): IGetRowResult {
         val ctx = Env.getCtx()
         val ad_Client_ID = Env.getAD_Client_ID(ctx)
@@ -221,42 +220,41 @@ class DataService : IDataService {
         val ad_User_ID = Env.getAD_User_ID(ctx)
         val cnn = DB.getConnectionRO()
 
-        set_user(cnn, ad_User_ID)
-
         val tableNameLowerCase = tableName.toLowerCase()
         val selectPart = "SELECT *"
-        val sql =  "$selectPart, set_user(?) FROM \"${tableNameLowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) AND \"${tableNameLowerCase}_id\" = ? "
+        val sql = "$selectPart, ? as ad_user_id FROM \"${tableNameLowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0 OR ?=0) AND \"${tableNameLowerCase}_id\" = ? "
         val statement = cnn.prepareStatement(sql)
         statement.setInt(1, ad_User_ID)
         statement.setInt(2, ad_Client_ID)
         statement.setInt(3, ad_Org_ID)
-        statement.setInt(4, id)
+        statement.setInt(4, ad_Org_ID)
+        statement.setInt(5, id)
         val rs = statement.executeQuery()
 
-        val table = MTable.get( ctx, tableName )
-        if ( table == null ) {
-            return GetRowResult( rs = rs, __metadata = null, __paging = null )
+        val table = MTable.get(ctx, tableName)
+        if (table == null) {
+            return GetRowResult(rs = rs, __metadata = null, __paging = null, rsmdColumnCount = rs.metaData.columnCount, cnn = cnn)
         } else {
             rs.next()
-            val modelFactory : IModelFactory = DefaultModelFactory()
-            val result = modelFactory.getPO( tableName, rs, "getRow")
-            return GetRowResultObj( o = result, __metadata = null, __paging = null )
+            val modelFactory: IModelFactory = DefaultModelFactory()
+            val result = modelFactory.getPO(tableName, rs, "getRow")
+            return GetRowResultObj(o = result, __metadata = null, __paging = null, rsmdColumnCount = 0, cnn = cnn)
         }
     }
 
-
     override fun getData(
-            connection: Connection, tableName: String,
-            columnsRequested : Array<String>?, // null => *
-            orderBy : String , // Name
-            orderByOrder : String , // ASC | DESC
-            offset : Int, // 0
-            limit : Int, // 100
-            filterName1: String, // Name
-            filterValue1: String, // Franta
-            filterName2: String, // LastName
-            filterValue2: String, // Vokurka
-            anonymous_call : Boolean
+        connection: Connection,
+        tableName: String,
+        columnsRequested: Array<String>?, // null => *
+        orderBy: String, // Name
+        orderByOrder: String, // ASC | DESC
+        offset: Int, // 0
+        limit: Int, // 100
+        filterName1: String, // Name
+        filterValue1: String, // Franta
+        filterName2: String, // LastName
+        filterValue2: String, // Vokurka
+        anonymous_call: Boolean
     ): IGetDataResult {
         val ctx = Env.getCtx()
         val ad_Client_ID = Env.getAD_Client_ID(ctx)
@@ -266,29 +264,29 @@ class DataService : IDataService {
         var count = 0
 
         val where_clause =
-                if ( filterName1 != "" ) {
+                if (filterName1 != "") {
                     " AND \"$filterName1\"=? " +
 
-                            if ( filterName2 != "" ) {
+                            if (filterName2 != "") {
                                 " AND \"$filterName2\"=? "
                             } else {
                                 ""
                             }
-
                 } else {
                     ""
                 }
 
         val tableName_lowerCase = tableName.toLowerCase()
 
-        val sql_count = "SELECT COUNT(*), set_user(?) FROM \"${tableName_lowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) $where_clause"
+        val sql_count = "SELECT COUNT(*), ? as ad_user_id FROM \"${tableName_lowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0 OR ?=0) $where_clause"
         val statement_count = cnn.prepareStatement(sql_count)
         statement_count.setInt(1, ad_User_ID)
         statement_count.setInt(2, ad_Client_ID)
         statement_count.setInt(3, ad_Org_ID)
-        if ( filterName1 != "" ) {
-            statement_count.setString( 3, filterValue1 );
-            if ( filterName2 != "" ) { statement_count.setString( 4, filterValue2 )  }
+        statement_count.setInt(4, ad_Org_ID)
+        if (filterName1 != "") {
+            statement_count.setString(5, filterValue1)
+            if (filterName2 != "") { statement_count.setString(6, filterValue2) }
         }
 
         val rs_count = statement_count.executeQuery()
@@ -297,42 +295,40 @@ class DataService : IDataService {
         }
 
         var selectPart =
-          if ( columnsRequested == null || columnsRequested!!.count() == 0 )  { "SELECT * " }
-          else
-            { columnsRequested.fold( "SELECT ", { total, next -> "$total \"$next\"," } ).trimEnd(',') }
+          if (columnsRequested == null || columnsRequested.count() == 0) { "SELECT * " } else { columnsRequested.fold("SELECT ", { total, next -> "$total \"$next\"," }).trimEnd(',') }
 
         val sql =
-                "$selectPart, set_user(?)  FROM \"${tableName_lowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) $where_clause" +
-                if ( orderBy != "" ) {
-                    " ORDER BY \"$orderBy\"" + if ( orderByOrder.toLowerCase() == "desc" ) { " desc" } else { "" }
+                "$selectPart, ? as ad_user_id  FROM \"${tableName_lowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0 OR ?=0) $where_clause" +
+                if (orderBy != "") {
+                    " ORDER BY \"$orderBy\"" + if (orderByOrder.toLowerCase() == "desc") { " desc" } else { "" }
                 } else { " ORDER BY 1" } +
                 " LIMIT $limit OFFSET $offset;"
         val statement = cnn.prepareStatement(sql)
         statement.setInt(1, ad_User_ID)
         statement.setInt(2, ad_Client_ID)
         statement.setInt(3, ad_Org_ID)
-        if ( filterName1 != "" ) {
-            statement.setString( 3, filterValue1 );
-            if ( filterName2 != "" ) { statement.setString( 4, filterValue2 )  }
+        statement.setInt(4, ad_Org_ID)
+        if (filterName1 != "") {
+            statement.setString(5, filterValue1)
+            if (filterName2 != "") { statement.setString(6, filterValue2) }
         }
         val rs = statement.executeQuery()
 
-        return GetDataResult( rs = rs, __metadata = null, __paging = Paging( count ) )
+        val result = GetDataResult(rs = rs, __metadata = null, __paging = Paging(count), rsmdColumnCount = rs.metaData.columnCount, cnn = cnn)
+        return result
     }
-
 }
 
 @Component
 class DataServiceRegisterHolder {
     companion object {
         var DataServiceRegister: IDataServiceRegister? = null
-        var dataService : DataService = DataService()
+        var dataService: DataService = DataService()
     }
 
     @Reference
     fun setDataServiceRegister(dataServiceRegister: IDataServiceRegister) {
         DataServiceRegister = dataServiceRegister
-        dataServiceRegister.registerService( dataService )
+        dataServiceRegister.registerService(dataService)
     }
-
 }
