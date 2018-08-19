995d22e1e1b1f2ddc76ab8294f3ce0354dd82c25
davidpodhola <david@podhola.net>
2018-08-19 07:47:19 +0200

    before install script separate

---
+++ before_install.sh
@@ -0,0 +1,4 @@
+wget https://sourceforge.net/projects/idempiereksys/files/idempiere-docker-image/idempiere-docker-pgsql-5.1.0.latest.tar.gz
+zcat idempiere-docker-pgsql-5.1.0.latest.tar.gz | docker load
+docker volume create --name idempiere-pgsql-datastore
+docker run -d -v idempiere-pgsql-datastore:/data -p 5433:5432 -e PASS="postgres" idempiere/idempiere-docker-pgsql:5.1.0.latest
