f1ec953c44b57ed54b5513391911ff7444ac4e04
davidpodhola <david@podhola.net>
2018-08-23 07:47:50 +0200

    Squashed 'subprojects/org.compiere.bo/' changes from a7ce63a..6258573
    6258573 warnings removed, code cleaned
    
    git-subtree-dir: subprojects/org.compiere.bo
    git-subtree-split: 62585731300bf948d2ae95e745d69e24e3144f47

--- src/kotlin/org/compiere/bo/UserService.kt
+++ src/kotlin/org/compiere/bo/UserService.kt
@@ -4,7 +4,7 @@ import org.compiere.crm.MUser
 import org.osgi.service.component.annotations.Component
 import software.hsharp.core.models.IUser
 import software.hsharp.core.services.IUserServiceImpl
-import java.util.*
+import java.util.Properties
 
 @Component
 class UserService : IUserServiceImpl {
