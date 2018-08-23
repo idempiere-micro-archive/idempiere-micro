a23d12e19099e8653376d15525cfb08f34d431af
davidpodhola <david@podhola.net>
2018-08-23 17:37:49 +0200

    test script fix

--- __test.sh
+++ __test.sh
@@ -1,7 +1,7 @@
 set -e 
 set -o pipefail
 (cd ./runtime;java -Dgosh.args=--nointeractive -jar ./bin/felix.jar &)
-gradle clean test findbugsMain build
+gradle clean test build
 cd integration_tests
 npm i
 ./run.sh
