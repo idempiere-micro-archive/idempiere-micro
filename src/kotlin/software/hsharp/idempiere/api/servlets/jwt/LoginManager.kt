93ca1589bbade59982edb227607861c74b74a9bb
davidpodhola <david@podhola.net>
2018-08-23 07:48:34 +0200

    Squashed 'subprojects/software.hsharp.idempiere.api/' changes from 576f627..7860f2f
    7860f2f warnings removed, code cleaned
    
    git-subtree-dir: subprojects/software.hsharp.idempiere.api
    git-subtree-split: 7860f2f047f34cacc6a047baa9d6f6f8cbe731f0

--- src/kotlin/software/hsharp/idempiere/api/servlets/jwt/LoginManager.kt
+++ src/kotlin/software/hsharp/idempiere/api/servlets/jwt/LoginManager.kt
@@ -1,10 +1,19 @@
 package software.hsharp.idempiere.api.servlets.jwt
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.idempiere.common.util.*
+import org.idempiere.common.util.CLogger
+import org.idempiere.common.util.DB
+import org.idempiere.common.util.Env
+import org.idempiere.common.util.KeyNamePair
+import org.idempiere.common.util.Language
+import org.idempiere.common.util.Trx
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
-import software.hsharp.api.helpers.jwt.*
+import software.hsharp.api.helpers.jwt.ILogin
+import software.hsharp.api.helpers.jwt.ILoginService
+import software.hsharp.api.helpers.jwt.ILoginServiceRegister
+import software.hsharp.api.helpers.jwt.ILoginResponse
+import software.hsharp.api.helpers.jwt.JwtManager
 import software.hsharp.idempiere.api.servlets.services.LoginService
 import software.hsharp.idempiere.api.servlets.services.SystemService
 import software.hsharp.idempiere.api.servlets.services.UserService
@@ -13,48 +22,44 @@ import java.sql.ResultSet
 import java.sql.SQLException
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Properties
 import java.util.logging.Level
 
 @Component
 class LoginServiceRegisterHolder {
-	companion object {
-	    var LoginServiceRegister: ILoginServiceRegister? = null
-		var loginManager : LoginManager = LoginManager()
-	}
+    companion object {
+        var LoginServiceRegister: ILoginServiceRegister? = null
+        var loginManager: LoginManager = LoginManager()
+    }
 
-	@Reference
-	fun setLoginServiceRegister(loginServiceRegister: ILoginServiceRegister) {
-		LoginServiceRegister = loginServiceRegister
-		loginServiceRegister.registerService( loginManager )
-	}
-
+    @Reference
+    fun setLoginServiceRegister(loginServiceRegister: ILoginServiceRegister) {
+        LoginServiceRegister = loginServiceRegister
+        loginServiceRegister.registerService(loginManager)
+    }
 }
 
 class LoginManager : ILoginService {
-	override val name: String
-		get() = "iDempiere Login Service"
-	override val uniqueKey: String
-		get() = LOGIN_MANAGER_UNIQUE_KEY
+    override val name: String
+        get() = "iDempiere Login Service"
+    override val uniqueKey: String
+        get() = LOGIN_MANAGER_UNIQUE_KEY
 
-	override fun login(login: ILogin): ILoginResponse {
-		return doLogin( login )
-	}
+    override fun login(login: ILogin): ILoginResponse {
+        return doLogin(login)
+    }
 
-	val LOGIN_MANAGER_UNIQUE_KEY  = "software.hsharp.idempiere.api.servlets.jwt.LoginManager"
-	val AUTH_HEADER_KEY = "Authorization"
-	val AUTH_HEADER_VALUE_PREFIX = "Bearer " // with trailing space to separate token
+    val LOGIN_MANAGER_UNIQUE_KEY = "software.hsharp.idempiere.api.servlets.jwt.LoginManager"
 
-	private val log = CLogger.getCLogger(LoginManager::class.java)
+    private val log = CLogger.getCLogger(LoginManager::class.java)
 
-	val dateFormatOnlyForCtx = "yyyy-MM-dd"
+    val dateFormatOnlyForCtx = "yyyy-MM-dd"
 
-	val C_BPARTNER_ID = "#C_BPartner_Id"
+    val C_BPARTNER_ID = "#C_BPartner_Id"
 
-
-	/**
+    /**
 	 * Check Login information and set context.
-	 * @returns    true if login info are OK
+	 * @returns true if login info are OK
 	 * @param ctx context
 	 * @param AD_User_ID user
 	 * @param AD_Role_ID role
@@ -62,65 +67,64 @@ class LoginManager : ILoginService {
 	 * @param AD_Org_ID org
 	 * @param M_Warehouse_ID warehouse
 	 */
-	private fun checkLogin(ctx: Properties, AD_User_ID: Int, AD_Role_ID: Int, AD_Client_ID: Int, AD_Org_ID: Int, M_Warehouse_ID: Int): KeyNamePair? {
-		//  Get Login Info
-		var loginInfo: String? = null
-		var c_bpartner_id = -1
-		//  Verify existence of User/Client/Org/Role and User's access to Client & Org
-		val sql = ("SELECT u.Name || '@' || c.Name || '.' || o.Name AS Text, u.c_bpartner_id, ? as ad_user_id " //#1
-				+ "FROM AD_User u, AD_Client c, AD_Org o, AD_User_Roles ur "
-				+ "WHERE u.AD_User_ID=?"    //  #2
+    private fun checkLogin(ctx: Properties, AD_User_ID: Int, AD_Role_ID: Int, AD_Client_ID: Int, AD_Org_ID: Int, M_Warehouse_ID: Int): KeyNamePair? {
+        //  Get Login Info
+        var loginInfo: String? = null
+        var c_bpartner_id = -1
+        //  Verify existence of User/Client/Org/Role and User's access to Client & Org
+        val sql = ("SELECT u.Name || '@' || c.Name || '.' || o.Name AS Text, u.c_bpartner_id, ? as ad_user_id " + // #1
+                "FROM AD_User u, AD_Client c, AD_Org o, AD_User_Roles ur " +
+                "WHERE u.AD_User_ID=?" + //  #2
 
-				+ " AND c.AD_Client_ID=?"   //  #3
+                " AND c.AD_Client_ID=?" + //  #3
 
-				+ " AND o.AD_Org_ID=?"      //  #4
+                " AND o.AD_Org_ID=?" + //  #4
 
-				+ " AND ur.AD_Role_ID=?"    //  #5
+                " AND ur.AD_Role_ID=?" + //  #5
 
-				+ " AND ur.AD_User_ID=u.AD_User_ID"
-				+ " AND (o.AD_Client_ID = 0 OR o.AD_Client_ID=c.AD_Client_ID)"
-				+ " AND c.AD_Client_ID IN (SELECT AD_Client_ID FROM AD_Role_OrgAccess ca WHERE ca.AD_Role_ID=ur.AD_Role_ID)"
-				+ " AND o.AD_Org_ID IN (SELECT AD_Org_ID FROM AD_Role_OrgAccess ca WHERE ca.AD_Role_ID=ur.AD_Role_ID) ")
-		var pstmt: PreparedStatement? = null
-		var rs: ResultSet? = null
-    	try {
-			pstmt = DB.prepareStatement(sql, null)
-			pstmt!!.setInt(1, AD_User_ID)
-			pstmt.setInt(2, AD_User_ID)
-			pstmt.setInt(3, AD_Client_ID)
-			pstmt.setInt(4, AD_Org_ID)
-			pstmt.setInt(5, AD_Role_ID)
-			rs = pstmt.executeQuery()
-			if (rs!!.next()) {
-				loginInfo = rs.getString(1)
-				c_bpartner_id = rs.getInt(2)
-			}
-		} catch (e: SQLException) {
-			e.printStackTrace()
-		} finally {
-			DB.close(rs, pstmt)
-		}
+                " AND ur.AD_User_ID=u.AD_User_ID" +
+                " AND (o.AD_Client_ID = 0 OR o.AD_Client_ID=c.AD_Client_ID)" +
+                " AND c.AD_Client_ID IN (SELECT AD_Client_ID FROM AD_Role_OrgAccess ca WHERE ca.AD_Role_ID=ur.AD_Role_ID)" +
+                " AND o.AD_Org_ID IN (SELECT AD_Org_ID FROM AD_Role_OrgAccess ca WHERE ca.AD_Role_ID=ur.AD_Role_ID) ")
+        var pstmt: PreparedStatement? = null
+        var rs: ResultSet? = null
+        try {
+            pstmt = DB.prepareStatement(sql, null)
+            pstmt!!.setInt(1, AD_User_ID)
+            pstmt.setInt(2, AD_User_ID)
+            pstmt.setInt(3, AD_Client_ID)
+            pstmt.setInt(4, AD_Org_ID)
+            pstmt.setInt(5, AD_Role_ID)
+            rs = pstmt.executeQuery()
+            if (rs!!.next()) {
+                loginInfo = rs.getString(1)
+                c_bpartner_id = rs.getInt(2)
+            }
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            DB.close(rs, pstmt)
+        }
 
-		//  not verified
-		if (loginInfo == null)
-			return null
+        //  not verified
+        if (loginInfo == null)
+            return null
 
-		//  Set Preferences
-		val org = KeyNamePair(AD_Org_ID, AD_Org_ID.toString())
-		var wh: KeyNamePair? = null
-		if (M_Warehouse_ID > 0)
-			wh = KeyNamePair(M_Warehouse_ID, M_Warehouse_ID.toString())
-		//
-		val date: Timestamp? = null
-		val printer: String? = null
-		val login = LoginService.loginUtility.init(ctx)
-		login.loadPreferences(org, wh, date, printer)
-		//
-		return KeyNamePair(c_bpartner_id, loginInfo)
-	}   //  checkLogin
+        //  Set Preferences
+        val org = KeyNamePair(AD_Org_ID, AD_Org_ID.toString())
+        var wh: KeyNamePair? = null
+        if (M_Warehouse_ID > 0)
+            wh = KeyNamePair(M_Warehouse_ID, M_Warehouse_ID.toString())
+        //
+        val date: Timestamp? = null
+        val printer: String? = null
+        val login = LoginService.loginUtility.init(ctx)
+        login.loadPreferences(org, wh, date, printer)
+        //
+        return KeyNamePair(c_bpartner_id, loginInfo)
+    } //  checkLogin
 
-
-	/**
+    /**
 	 *
 	 * @param AD_User_ID
 	 * @param AD_Role_ID
@@ -130,30 +134,30 @@ class LoginManager : ILoginService {
 	 * @param Lang
 	 * @return true if login is successful
 	 */
-	fun login(ctx: Properties, AD_User_ID: Int, AD_Role_ID: Int, AD_Client_ID: Int, AD_Org_ID: Int, M_Warehouse_ID: Int, Lang: String): Boolean {
-		val loginInfoFull = checkLogin(ctx, AD_User_ID, AD_Role_ID, AD_Client_ID, AD_Org_ID, M_Warehouse_ID)
-				?: return false
-		val c_bpartner_id = loginInfoFull.key
+    fun login(ctx: Properties, AD_User_ID: Int, AD_Role_ID: Int, AD_Client_ID: Int, AD_Org_ID: Int, M_Warehouse_ID: Int, Lang: String): Boolean {
+        val loginInfoFull = checkLogin(ctx, AD_User_ID, AD_Role_ID, AD_Client_ID, AD_Org_ID, M_Warehouse_ID)
+                ?: return false
+        val c_bpartner_id = loginInfoFull.key
 
-		Env.setContext(ctx, "#AD_Language", Lang)
-		val m_language = Language.getLanguage(Lang)
-		Env.verifyLanguage(ctx, m_language)
+        Env.setContext(ctx, "#AD_Language", Lang)
+        val m_language = Language.getLanguage(Lang)
+        Env.verifyLanguage(ctx, m_language)
 
-		//  Set Date
-		val ts = Timestamp(System.currentTimeMillis())
+        //  Set Date
+        val ts = Timestamp(System.currentTimeMillis())
 
-		val dateFormat4Timestamp = SimpleDateFormat(dateFormatOnlyForCtx)
-		Env.setContext(ctx, "#Date", dateFormat4Timestamp.format(ts) + " 00:00:00")    //  JDBC format
-		if (log.isLoggable(Level.INFO)) log.info(" #Date = " + Env.getContextAsDate(ctx, "#Date"))
+        val dateFormat4Timestamp = SimpleDateFormat(dateFormatOnlyForCtx)
+        Env.setContext(ctx, "#Date", dateFormat4Timestamp.format(ts) + " 00:00:00") //  JDBC format
+        if (log.isLoggable(Level.INFO)) log.info(" #Date = " + Env.getContextAsDate(ctx, "#Date"))
 
-		Env.setContext(ctx, "#M_Warehouse_ID", M_Warehouse_ID)
-		Env.setContext(ctx, Env.LANGUAGE, m_language.aD_Language)
-		Env.setContext(ctx, C_BPARTNER_ID, c_bpartner_id)
+        Env.setContext(ctx, "#M_Warehouse_ID", M_Warehouse_ID)
+        Env.setContext(ctx, Env.LANGUAGE, m_language.aD_Language)
+        Env.setContext(ctx, C_BPARTNER_ID, c_bpartner_id)
 
-		return true
-	}
+        return true
+    }
 
-	fun doLogin( login : ILogin ) : UserLoginModelResponse {
+    fun doLogin(login: ILogin): UserLoginModelResponse {
         SystemService.system.startup()
         Trx.startTrxMonitor(SystemService.system.getThreadPoolExecutor())
 
@@ -168,12 +172,10 @@ class LoginManager : ILoginService {
         val clients = loginUtil.getClients(login.loginName, login.password)
 
         val selectedClientIndex =
-				if (clients == null) { -1 } else { clients.indexOfFirst { clients.count() == 1 || it.Key == login.clientId } }
+                if (clients == null) { -1 } else { clients.indexOfFirst { clients.count() == 1 || it.Key == login.clientId } }
 
         val roles =
-            if (selectedClientIndex == -1) {
-                null
-            } else {
+            if (selectedClientIndex == -1) null else {
                 val clientId = clients[selectedClientIndex].ID
                 ctx.setProperty(Env.AD_CLIENT_ID, clientId)
                 Env.setContext(ctx, Env.AD_CLIENT_ID, clientId)
@@ -199,7 +201,7 @@ class LoginManager : ILoginService {
                 if (selectedRoleIndex == -1) {
                     null
                 } else {
-                    loginUtil.getOrgs(roles!![selectedRoleIndex]);
+                    loginUtil.getOrgs(roles!![selectedRoleIndex])
                 }
 
             val selectedOrgIndex =
@@ -214,7 +216,7 @@ class LoginManager : ILoginService {
                 if (selectedOrgIndex == -1) {
                     null
                 } else {
-                    loginUtil.getWarehouses(orgs!![selectedOrgIndex]);
+                    loginUtil.getWarehouses(orgs!![selectedOrgIndex])
                 }
 
             val selectedWarehouseIndex =
