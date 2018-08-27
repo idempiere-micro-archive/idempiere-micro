dc160c2e751e7034250111776da02312d2bf9f17
davidpodhola <david@podhola.net>
2018-08-27 21:05:38 +0200

    cleaned

--- clones.sh
+++
@@ -1,7 +0,0 @@
-./clone_idempiere_module.sh osgi.software.hsharp.db.postgresql.provider
-./clone_idempiere_module.sh osgi.org.idempiere.common
-./clone_idempiere_module.sh osgi.org.idempiere.orm
-./clone_idempiere_module.sh osgi.software.hsharp.business.core
-./clone_idempiere_module.sh osgi.org.compiere.crm
-./clone_idempiere_module.sh osgi.org.compiere.db.postgresql.provider
-./clone_idempiere_module.sh osgi.org.idempiere.app
